8785603f5e7b2e98d5cab8e41fe2e4deec2903a9
Hannah Ashton <h.charise18@gmail.com>
2024-09-03 11:13:01 -0600

    Added function to parse emotions and print into predictions

---
+++ parse_emotion.py
@@ -0,0 +1,19 @@
+
+def parse_emotion(image_path):
+    # print out the true emotion and check if correct or not
+    if image_path.find("angry") == -1:
+        emotion = "angry"
+    elif image_path.find("disgust") == -1:
+        emotion = "disgust"
+    elif image_path.find("fear") == -1:
+        emotion = "fear"
+    elif image_path.find("neutral") == -1:
+        emotion = "neutral"
+    elif image_path.find("sad") == -1:
+        emotion = "sad"
+    elif image_path.find("surprise") == -1:
+        emotion = "surprise"
+    elif image_path.find("happy") == -1:
+        emotion = "happy"
+
+    return emotion
