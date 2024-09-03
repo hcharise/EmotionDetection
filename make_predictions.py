aa1297bbe20241fa43119e4335340d2da2013d29
Hannah Ashton <h.charise18@gmail.com>
2024-09-03 12:05:16 -0600

    Updated comments/documentation in make_predictions

--- make_predictions.py
+++ make_predictions.py
@@ -1,15 +1,21 @@
 from deepface import DeepFace
 from parse_emotion import *
 
+# Function to attempt deepface analysis and print results if successful, error message if not
 def make_predictions(img, file):
     try:
+        # Detect face and recognize emotion/gender
         face_analysis = DeepFace.analyze(img)
+
+        # Write results
         file.write(img)
         file.write(", " + face_analysis[0]['dominant_gender'])
         file.write(", " + face_analysis[0]['dominant_emotion'])
         file.write(", " + parse_emotion(img))
         file.write("\n")
+
     except:
+        # If unable to detect face, write error message
         file.write(img)
         file.write(", COULD NOT DETECT FACE")
         file.write("\n")
